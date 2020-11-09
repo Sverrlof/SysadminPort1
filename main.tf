49374cee87222230421d88e38cef9811bdaea72b
Sverre Lofthus <sverrlof@stud.ntnu.no>
2020-11-09 16:42:27 +0100

    created three instances
    Three instances. Just for testing

---
+++ main.tf
@@ -0,0 +1,32 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_access_key
+}
+
+variable "access_key" {
+  type        = string
+}
+
+variable "secret_access_key" {
+  type        = string
+}
+
+resource "aws_instance" "server-one" {
+  ami           = "ami-0dba2cb6798deb6d8"
+  instance_type = "t2.micro"
+  }
+
+resource "aws_instance" "server-two" {
+  ami           = "ami-0dba2cb6798deb6d8"
+  instance_type = "t2.micro"
+}
+
+resource "aws_instance" "server-three" {
+  ami           = "ami-0dba2cb6798deb6d8"
+  instance_type = "t2.micro"
+}
+
+
+
